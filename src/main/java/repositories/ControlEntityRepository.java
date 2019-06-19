9aab0a52856d48657ec83a1e345c8e7772422864
David Gil Baeza <davidgil.univ@gmail.com>
2019-06-19 17:39:58 +0200

    Repositorio terminado

--- src/main/java/repositories/ControlEntityRepository.java
+++ src/main/java/repositories/ControlEntityRepository.java
@@ -25,22 +25,22 @@ public interface ControlEntityRepository extends JpaRepository<ControlEntity, In
 	@Query("select c from Position p join p.controlEntity c where p.id = ?1 and p.isDraftMode = false")
 	List<ControlEntity> getAllControlEntityOfPosition(Integer positionId);
 
-//	@Query("select c from Company co join co.positions p join p.applications a join a.controlEntity c where co.id = ?1 and (a.status ='ACCEPTED' or a.status = 'REJECTED')")
-//	List<ControlEntity> getAllControlEntityOfCompany(Integer companyId);
-//
-//	@Query("select a from Company c join c.positions p join p.applications a where c.id = ?1 and a.id = ?2")
-//	Application checkCompanyAndApplication(Integer companyId, Integer applicationId);
-//	
-//	@Query("select c.ticker from ControlEntity c")
-//	List<String> getAllTickers();
-//
-//	@Query("select c from Company co join co.positions p join p.applications a join a.controlEntity c where co.id = ?1 and a.id = ?2")
-//	List<ControlEntity> getAllControlEntityOfCompanyAndApplication(Integer companyId, Integer applicationId);
-//
-//	@Query("select c from Company co join co.positions p join p.applications a join a.controlEntity c where co.id = ?1 and c.id = ?2 and c.isDraftMode = true")
-//	ControlEntity checkCompanyAndControlEntity(Integer companyId, Integer controlEntityId);
-//
-//	@Query("select a from Application a join a.controlEntity c where c.id = ?1")
-//	Application getApplicationOfControlEntity(Integer controlEntityId);
+	@Query("select c from Company co join co.positions p join p.controlEntity c where co.id = ?1 and p.isDraftMode = false")
+	List<ControlEntity> getAllControlEntityOfCompany(Integer companyId);
+
+	@Query("select p from Company c join c.positions p where c.id = ?1 and p.id = ?2")
+	Position checkCompanyAndPosition(Integer companyId, Integer positionId);
+	
+	@Query("select c.ticker from ControlEntity c")
+	List<String> getAllTickers();
+
+	@Query("select c from Company co join co.positions p join p.controlEntity c where co.id = ?1 and p.id = ?2")
+	List<ControlEntity> getAllControlEntityOfCompanyAndPosition(Integer companyId, Integer positionId);
+
+	@Query("select c from Company co join co.positions p join p.controlEntity c where co.id = ?1 and c.id = ?2 and c.isDraftMode = true")
+	ControlEntity checkCompanyAndControlEntity(Integer companyId, Integer controlEntityId);
+
+	@Query("select p from Position p join p.controlEntity c where c.id = ?1")
+	Position getPositionOfControlEntity(Integer controlEntityId);
 
 }
